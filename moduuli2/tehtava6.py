39172dbafbc4c45e65ad525d9003ec847d618c20
sonjameskus <sonja.meskus@metropolia.fi>
2025-01-16 14:05:13 +0200

    lisäsin moduuli 2 tehtävä 6

---
+++ moduuli2/tehtava6.py
@@ -0,0 +1,25 @@
+import random
+no1 = random.randint(0,9)
+no1 = str(no1)
+
+no2 = random.randint(0,9)
+no2 = str(no2)
+
+no3 = random.randint(0,9)
+no3 = str(no3)
+
+print (no1 + no2 + no3)
+
+no4 = random.randint(1,6)
+no4 = str(no4)
+
+no5 = random.randint(1,6)
+no5 = str(no5)
+
+no6 = random.randint(1,6)
+no6 = str(no6)
+
+no7 = random.randint(1,6)
+no7 = str(no7)
+
+print (no4 + no5 + no6 + no7)
