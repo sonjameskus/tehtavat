0d8243952341ba17964f825699dc970b983dc5df
sonjameskus <sonja.meskus@metropolia.fi>
2025-01-17 11:18:02 +0200

    lisäsin moduuli 3 tehtävä 1

---
+++ moduuli3/tehtava1.py
@@ -0,0 +1,5 @@
+kuha = float(input("Anna kuhan pituus senttimetreinä: "))
+lasku = 37-kuha
+tulos = float(lasku)
+if lasku>0:
+    print("Kuhan sallitusta pyyntimitasta puuttuu " + str(tulos) + "cm ja se täytyy laskea takaisin järveen.")
